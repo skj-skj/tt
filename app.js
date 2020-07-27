210d7f4f233972008c6b361173a34f2507b18c4e
SKJ <sripal.17jccs069@jietjodhpur.ac.in>
2020-07-27 12:38:52 +0530

    Added Link Functionality

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ function getWeekDay() {
     return weekDay;
 }
 
+// function copyToClipboard(text) {
+//     var dummy = document.createElement("textarea");
+//     // to avoid breaking orgain page when copying more words
+//     // cant copy when adding below this code
+//     // dummy.style.display = 'none'
+//     document.body.appendChild(dummy);
+//     //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
+//     dummy.value = text;
+//     dummy.select();
+//     document.execCommand("copy");
+//     document.body.removeChild(dummy);
+// }
+
 timeTable = [
 
     ['Monday','B2 PROJECT(KP)','EEDM(SU) or QM(RJ)','B2 SEMINAR(RJ)'],
@@ -24,28 +37,69 @@ timeTable = [
     ['Sunday','Its Sunday','Its Sunday','Its Sunday'],
 
 ];
+
+meetLinks = {
+    'B2 PROJECT(KP)': 'https://meet.google.com/lookup/danco6xf7j?authuser=2',
+    'B2 SEMINAR(RJ)': 'https://meet.google.com/lookup/c7mwo6raen?authuser=2',
+    'EEDM(SU)': 'https://meet.google.com/lookup/esfasmvt7s?authuser=2',
+    'QM(RJ)': 'https://meet.google.com/lookup/f43zwuvuqk?authuser=2',
+    'IOT(RB)': '#',
+    'VERBAL (Prof. Mamta Sharma)': '#',
+    'VERBAL (Prof. Bhavani Singh)': '#',
+    'QUANT (Prof. Abhishek Karwa)': '#'
+}
 let lecCount = 1;
 let weekDay = getWeekDay();
 
 timeTable.forEach(element => {
     if(element[0]==weekDay){
         element.forEach(lec => {
-            if(lec != weekDay){
-                let mainDivBody = document.querySelector('div.lectures');
-                let aTag = document.createElement('a');
-                aTag.className = 'collection-item';
-                aTag.href = '#';
-                aTag.textContent = `${lecCount}) ${lec}`;
-                mainDivBody.appendChild(aTag);
-                lecCount++;                
-            }
-            else{
+            if(lec == weekDay){
                 let mainDivBody = document.querySelector('div#weekDay');
                 let aTag = document.createElement('a');
-                aTag.className = 'collection-item';
+                aTag.className = 'lecItem';
                 aTag.href = '#';
                 aTag.textContent = `${lec}:`
+
+                
+                mainDivBody.appendChild(aTag);    
+            }
+            else if(lec == 'EEDM(SU) or QM(RJ)'){
+                let mainDivBody = document.querySelector('div.lectures');
+                let aTagEEDM = document.createElement('a');
+                let aTagQM = document.createElement('a');
+                aTagEEDM.href = meetLinks['EEDM(SU)'];
+                aTagEEDM.textContent = 'EEDM(SU)';
+                aTagEEDM.target = '_blank';
+
+                aTagQM.href = meetLinks['QM(RJ)'];
+                aTagQM.textContent = 'QM(RJ)';
+                aTagQM.target = '_blank';
+                let basePTag  = document.createElement('p');
+                basePTag.className = 'lecItemSplit'
+                basePTag.append(`${lecCount}) `);
+                basePTag.appendChild(aTagEEDM);
+                basePTag.append(' OR ');
+                basePTag.appendChild(aTagQM);
+
+                mainDivBody.appendChild(basePTag);
+
+
+
+            }
+            else{
+                let mainDivBody = document.querySelector('div.lectures');
+                let aTag = document.createElement('a');
+                aTag.className = 'lecItem';
+                aTag.target = '_blank';
+                aTag.href = meetLinks[lec];
+                aTag.textContent = `${lecCount}) ${lec}`;
+                
+            
                 mainDivBody.appendChild(aTag);
+                
+                
+                lecCount++;
             }
             
 
