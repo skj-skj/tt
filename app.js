eafbe871b6af2ceadd125fe007a90fb855187f7a
SKJ <sripal.17jccs069@jietjodhpur.ac.in>
2020-09-28 19:11:58 +0530

    Added New Feature 1)authuser button to set new value of authuser 2)Prev & Next Day Time Table

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ function getWeekDay() {
     return weekDay;
 }
 
+function getWeekDayOnlyNumber(){
+
+    return new Date().getDay();
+}
+
+function getWeekDayNameByNumber(num){
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+    return weekday[num];
+}
+
 function setVideo(){
     videoBanner = document.querySelector('video.video-banner');
     videoBanner.innerHTML = "";
@@ -25,18 +43,9 @@ function setVideo(){
     videoBanner.appendChild(sourceVid);
 }
 
-// function copyToClipboard(text) {
-//     var dummy = document.createElement("textarea");
-//     // to avoid breaking orgain page when copying more words
-//     // cant copy when adding below this code
-//     // dummy.style.display = 'none'
-//     document.body.appendChild(dummy);
-//     //Be careful if you use texarea. setAttribute('value', value), which works with "input" does not work with "textarea". – Eduard
-//     dummy.value = text;
-//     dummy.select();
-//     document.execCommand("copy");
-//     document.body.removeChild(dummy);
-// }
+//Logo P tag elemnet
+let logo = document.querySelector('div#weekDay').firstElementChild;
+
 
 
 /*
@@ -50,6 +59,9 @@ VERBAL (Prof. Bhavani Singh)
 QUANT (Prof. Abhishek Karwa)
 Its Sunday
 */
+//authuser
+let authuser = 2;
+
 timeTable = [
 
     ['Monday','EEDM(SU) or QM(RJ)','B2 PROJECT(KP)','B2 SEMINAR(RJ)'],
@@ -63,85 +75,134 @@ timeTable = [
 ];
 
 meetLinks = {
-    'B2 PROJECT(KP)': 'https://meet.google.com/lookup/danco6xf7j?authuser=2',
-    'B2 SEMINAR(RJ)': 'https://meet.google.com/lookup/c7mwo6raen?authuser=2',
-    'EEDM(SU)': 'https://meet.google.com/lookup/esfasmvt7s?authuser=2',
-    'QM(RJ)': 'https://meet.google.com/lookup/f43zwuvuqk?authuser=2',
-    'IOT(RB)': 'https://meet.google.com/lookup/cqbbzdwt6l?authuser=2',
-    'VERBAL (Prof. Mamta Sharma)': 'https://meet.google.com/lookup/ms-sst?authuser=2',
+    'B2 PROJECT(KP)': 'https://meet.google.com/lookup/danco6xf7j?authuser=',
+    'B2 SEMINAR(RJ)':'https://meet.google.com/lookup/eoehgugsdv?authuser=',
+    // 'B2 SEMINAR(RJ)': 'https://meet.google.com/lookup/c7mwo6raen?authuser=2',
+    'EEDM(SU)': 'https://meet.google.com/lookup/esfasmvt7s?authuser=',
+    'QM(RJ)': 'https://meet.google.com/lookup/f43zwuvuqk?authuser=',
+    'IOT(RB)': 'https://meet.google.com/lookup/cqbbzdwt6l?authuser=',
+    'VERBAL (Prof. Mamta Sharma)': 'https://meet.google.com/lookup/ms-sst?authuser=',
     'VERBAL (Prof. Bhavani Singh)': '#',
-    'QUANT (Prof. Abhishek Karwa)': 'https://meet.google.com/lookup/sst_cse_coe?authuser=2'
+    'QUANT (Prof. Abhishek Karwa)': 'https://meet.google.com/lookup/sst_cse_coe?authuser='
 }
-let lecCount = 1;
+
 
 let timing = ['0','09:00 AM - 09:50 AM','10:00 AM - 10:50 AM','12:20 AM - 1:10 PM'];
-let weekDay = getWeekDay();
+// let weekDay = getWeekDay();
 
-timeTable.forEach(element => {
-    if(element[0]==weekDay){
-        element.forEach(lec => {
-            if(lec == weekDay){
-                let mainDivBody = document.querySelector('div#weekDay');
-                let aTag = document.createElement('a');
-                aTag.className = 'lecItem weekDay';
-                aTag.href = '#';
-                aTag.textContent = `${lec}: (Updated)`
-
-                
-                mainDivBody.appendChild(aTag);    
-            }
-            else if(lec == 'EEDM(SU) or QM(RJ)'){
-                let mainDivBody = document.querySelector('div.lectures');
-                let aTagEEDM = document.createElement('a');
-                let aTagQM = document.createElement('a');
-                let spanTag = document.createElement('span');
-                spanTag.textContent = `{${timing[lecCount]}}`;
-
-                aTagEEDM.href = meetLinks['EEDM(SU)'];
-                aTagEEDM.textContent = 'EEDM(SU)';
-                aTagEEDM.target = '_blank';
-
-                aTagQM.href = meetLinks['QM(RJ)'];
-                aTagQM.textContent = 'QM(RJ)';
-                aTagQM.target = '_blank';
-                let basePTag  = document.createElement('p');
-                basePTag.className = 'lecItemSplit'
-                basePTag.appendChild(spanTag);
-                basePTag.append(document.createElement('br'));
-                basePTag.append(` ${lecCount}) `);
-                basePTag.appendChild(aTagEEDM);
-                basePTag.append(' OR ');
-                basePTag.appendChild(aTagQM);
-
-                mainDivBody.appendChild(basePTag);
-
-                lecCount++;
-
-            }
-            else{
-                let mainDivBody = document.querySelector('div.lectures');
-                let aTag = document.createElement('a');
-                let spanTag = document.createElement('span');
-                spanTag.textContent = `{${timing[lecCount]}}`;
-                aTag.className = 'lecItem';
-                aTag.target = '_blank';
-                aTag.href = meetLinks[lec];
-                aTag.appendChild(spanTag);
-                aTag.append(document.createElement('br'));
-                aTag.append(` ${lecCount}) ${lec}`);
-
-            
-                mainDivBody.appendChild(aTag);
-                
-                
-                lecCount++;
-            }
-            
-
-        })
-        
-
+function timeTableDisplay(weekDay){
+    // removing all element in lectures div
+    document.querySelector('div.lectures').innerHTML='';
+    document.querySelector('div#weekDay').innerHTML = logo.innerHTML;
+    
+    if(weekDay>6 || weekDay<0){
+        weekDay = Math.abs(weekDay%7);
     }
+
+    let lecCount = 1;
+    weekDay = getWeekDayNameByNumber(weekDay);
+
+    timeTable.forEach(element => {
+        if(element[0]==weekDay){
+            element.forEach(lec => {
+                if(lec == weekDay){
+                    let mainDivBody = document.querySelector('div#weekDay');
+                    let aTag = document.createElement('a');
+                    aTag.className = 'lecItem weekDay';
+                    aTag.href = '#';
+                    aTag.textContent = `${lec}: (Updated)`;
+
+                    mainDivBody.appendChild(aTag);
+                }
+                else if(lec == 'EEDM(SU) or QM(RJ)'){
+                    let mainDivBody = document.querySelector('div.lectures');
+                    let aTagEEDM = document.createElement('a');
+                    let aTagQM = document.createElement('a');
+                    let spanTag = document.createElement('span');
+                    spanTag.textContent = `{${timing[lecCount]}}`;
+    
+                    aTagEEDM.href = `${meetLinks['EEDM(SU)']}${authuser}`;
+                    aTagEEDM.textContent = 'EEDM(SU)';
+                    aTagEEDM.target = '_blank';
+    
+                    aTagQM.href = `${meetLinks['QM(RJ)']}${authuser}`;
+                    aTagQM.textContent = 'QM(RJ)';
+                    aTagQM.target = '_blank';
+                    let basePTag  = document.createElement('p');
+                    basePTag.className = 'lecItemSplit'
+                    basePTag.appendChild(spanTag);
+                    basePTag.append(document.createElement('br'));
+                    basePTag.append(` ${lecCount}) `);
+                    basePTag.appendChild(aTagEEDM);
+                    basePTag.append(' OR ');
+                    basePTag.appendChild(aTagQM);
+    
+                    mainDivBody.appendChild(basePTag);
+    
+                    lecCount++;
+    
+                }
+                else{
+                    let mainDivBody = document.querySelector('div.lectures');
+                    let aTag = document.createElement('a');
+                    let spanTag = document.createElement('span');
+                    spanTag.textContent = `{${timing[lecCount]}}`;
+                    aTag.className = 'lecItem';
+                    aTag.target = '_blank';
+                    aTag.href = `${meetLinks[lec]}${authuser}`;
+                    aTag.appendChild(spanTag);
+                    aTag.append(document.createElement('br'));
+                    aTag.append(` ${lecCount}) ${lec}`);
+    
+                
+                    mainDivBody.appendChild(aTag);
+                    
+                    
+                    lecCount++;
+                }
+                
+    
+            })
+            
+    
+        }
+    });
+    
+}
+
+let weekDayNumber = getWeekDayOnlyNumber();
+
+timeTableDisplay(weekDayNumber);
+setVideo();
+
+
+
+allAuthuser = document.querySelectorAll('footer ul.authuser li');
+
+allAuthuser.forEach(authUserElement => {
+    authUserElement.addEventListener('click', (e)=>{
+        authuser = parseInt(authUserElement.textContent);
+        timeTableDisplay(getWeekDayOnlyNumber());
+    });
 });
 
-setVideo();
+prevButton = document.querySelector('button.left');
+nextButton = document.querySelector('button.right');
+resetDivButton = document.querySelector('div#weekDay');
+
+prevButton.addEventListener('click',(e)=>{
+    weekDayNumber--;
+    timeTableDisplay(weekDayNumber);
+});
+
+nextButton.addEventListener('click',(e)=>{
+    weekDayNumber++;
+    timeTableDisplay(weekDayNumber);
+});
+
+resetDivButton.addEventListener('click',(e)=>{
+    timeTableDisplay(getWeekDayOnlyNumber());
+})
+
+
+
