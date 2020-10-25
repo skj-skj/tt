6ed0516711b5f3e87dfe4acb810b922e4a51f6a3
SKJ <sripal.17jccs069@jietjodhpur.ac.in>
2020-10-26 00:09:18 +0530

    Updated TimeTable

--- config/config.js
+++ config/config.js
@@ -17,10 +17,12 @@ CYBER SECURITY LAB (SU)
 IOT LAB (RB)
 */
 
+/*
 let timeTableObj = {
     "Monday":{
         "1": "CYBER SECURITY LAB (SU)",
-        "2": "Placement Training"
+        "2": "Placement Training",
+        
     },
     "Tuesday":{
         "1": "IOT LAB (RB)",
@@ -46,6 +48,45 @@ let timeTableObj = {
         "1": "No Class"
     }
 }
+*/
+
+
+let timeTableObj = {
+    "Monday":{
+        "1": "EEDM(SU) or QM(RJ)",
+        "2": "B2 PROJECT(KP)",
+        "3":"B2 SEMINAR[AC]"
+    },
+    "Tuesday":{
+        "1": "IOT(RB)",
+        "2": "B2 SEMINAR(AC)",
+        "3":"B2 PROJECT(KP)"
+    },
+    "Wednesday":{
+        "1": "EEDM(SU) or QM(RJ)",
+        "2": "B2 SEMINAR(AC)",
+        "3":"VERBAL (Prof. Mamta Sharma)"
+    },
+    "Thursday":{
+        "1": "IOT(RB)",
+        "2": "B2 PROJECT(KP)",
+        "3":"VERBAL (Prof. Bhavani Singh)"
+    },
+    "Friday":{
+        "1": "IOT(RB)",
+        "2": "EEDM(SU) or QM(RJ)",
+        "3":"QUANT (Prof. Abhishek Karwa)"
+    },
+    "Saturday":{
+        "1": "EEDM(SU) or QM(RJ)",
+        "2": "IOT(RB)",
+        "3":"QUANT (Prof. Abhishek Karwa)"
+    },
+    "Sunday":{
+        "1": "No Class"
+    }
+}
+
 
 /*
 these link are made by using nicknames:
@@ -57,13 +98,26 @@ By default authuser = 2
 */
 
 let meetLinks = {
+    'B2 PROJECT(KP)': 'https://meet.google.com/lookup/danco6xf7j?authuser=',
+    'B2 SEMINAR[AC]':'https://meet.google.com/lookup/eoehgugsdv?authuser=',
+    'B2 SEMINAR(AC)': 'https://meet.google.com/lookup/c7mwo6raen?authuser=',
+    'EEDM(SU)': 'https://meet.google.com/lookup/esfasmvt7s?authuser=',
+    'QM(RJ)': 'https://meet.google.com/lookup/f43zwuvuqk?authuser=',
+    'IOT(RB)': 'https://meet.google.com/lookup/cqbbzdwt6l?authuser=',
+    'VERBAL (Prof. Mamta Sharma)': 'https://meet.google.com/lookup/ms-sst?authuser=',
+    'VERBAL (Prof. Bhavani Singh)': '#',
+    'QUANT (Prof. Abhishek Karwa)': 'https://meet.google.com/lookup/sst_cse_coe?authuser=',
+    'No Class': 'https://youtube.com/'
+}
+/*
+let meetLinks = {
     'CYBER SECURITY LAB (SU)': 'https://meet.google.com/lookup/a75muhyyoa?authuser=',
     'IOT LAB (RB)':'https://meet.google.com/lookup/erowg3243c?authuser=',
     'Placement Training':'#',
     'No Class': 'https://youtube.com/'
-}
+}*/
 
-let timing = ['0','09:00 AM - 11:30 AM','12:30 PM - 1:20 PM'];
+let timing = ['0','09:00 AM - 09:50 AM','10:00 AM - 10:50 AM','1:20 PM - 2:10 PM'];
 
 let showVideos = true;
 
